8950de6a871fa066cf5e11cbb424d51765e2ce4f
harrynaoh8-prog <harrynaoh8@gmail.com>
2026-01-11 17:26:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+body {
+  font-family: Arial, sans-serif;
+  background: #f4f4f4;
+}
+
+.quiz-container {
+  background: white;
+  width: 350px;
+  margin: 50px auto;
+  padding: 20px;
+  border-radius: 10px;
+  text-align: center;
+}
+
+button {
+  width: 100%;
+  padding: 10px;
+  margin: 5px 0;
+  cursor: pointer;
+}
+
+#nextBtn {
+  display: none;
+  background: green;
+  color: white;
+}
 
